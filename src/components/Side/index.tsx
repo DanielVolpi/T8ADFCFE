34e06a1a66f32695bcf4bcaa22cb3be539f6bc65
Dann Oak <dann.inbox+github@proton.me>
2024-03-19 19:51:23 -0300

    feat: Specialist fetched from Backend

--- src/components/Side/index.tsx
+++ src/components/Side/index.tsx
@@ -1,32 +1,29 @@
 'use client';
 
-import getSpecialists from '../../services/getSpecialists';
 import getText from '../../services/getText';
 import StyledSide from './style';
+import useFetch from '@/hooks/useFetchSpecialists';
 import ISpecialist from '@/interfaces/ISpecialist';
 import SpecialistListing from '@/components/SpecialistListing';
-import { useEffect, useState } from 'react';
 
 function Side() {
   const { mainText1, mainText2, mainText3, mainText4, mainText5 } = getText();
-  const [specialists, setSpecialists] = useState<ISpecialist[]>([]);
+  const { data } = useFetch<ISpecialist[]>('http://localhost:4000/specialists');
 
-  useEffect(() => {
-    const fetchSpecialists = async () => {
-      setSpecialists(await getSpecialists());
-    };
+  // useEffect(() => {
+  //   const fetchSpecialists = async () => {
+  //     setSpecialists(await getSpecialists());
+  //   };
 
-    fetchSpecialists();
-  }, []);
+  //   fetchSpecialists();
+  // }, []);
 
   return (
     <StyledSide>
       <div>{mainText1}</div>
       <div>{mainText2}</div>
       <div>{mainText3}</div>
-      {specialists && specialists.length > 0 && (
-        <SpecialistListing entries={specialists} />
-      )}
+      {data && data.length > 0 && <SpecialistListing entries={data} />}
       <div>{mainText4}</div>
       <div>{mainText5}</div>
     </StyledSide>

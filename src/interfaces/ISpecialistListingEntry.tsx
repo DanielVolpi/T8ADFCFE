f3314e7456ff63864de81492faebc644954efcc6
Dann Oak <dann.inbox+github@proton.me>
2024-03-21 14:11:50 -0300

    fix: fixing interface

--- src/interfaces/ISpecialistListingEntry.tsx
+++ src/interfaces/ISpecialistListingEntry.tsx
@@ -2,7 +2,6 @@ import ISpecialist from '@/interfaces/ISpecialist';
 
 interface ISpecialistListingEntry {
   entries: ISpecialist[];
-  deleteCallback: (id: string) => void;
 }
 
 export default ISpecialistListingEntry;

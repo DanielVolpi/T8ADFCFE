63e25031190a5ad9d3b0da7a51adda574bba8974
Dann Oak <dann.inbox+github@proton.me>
2024-03-21 22:38:45 -0300

    feat: adding Unit Testing for User Form

---
+++ mockData/countries.js
@@ -0,0 +1,56 @@
+const countriesMockData = [
+  {
+    name: {
+      common: 'United States',
+      official: 'United States of America',
+      nativeName: {
+        eng: {
+          official: 'United States of America',
+          common: 'United States',
+        },
+      },
+    },
+  },
+  {
+    name: {
+      common: 'Denmark',
+      official: 'Kingdom of Denmark',
+      nativeName: {
+        dan: {
+          official: 'Kongeriget Danmark',
+          common: 'Danmark',
+        },
+      },
+    },
+  },
+  {
+    name: {
+      common: 'Australia',
+      official: 'Commonwealth of Australia',
+      nativeName: {
+        eng: {
+          official: 'Commonwealth of Australia',
+          common: 'Australia',
+        },
+      },
+    },
+  },
+  {
+    name: {
+      common: 'Argentina',
+      official: 'Argentine Republic',
+      nativeName: {
+        grn: {
+          official: 'Argentine Republic',
+          common: 'Argentina',
+        },
+        spa: {
+          official: 'República Argentina',
+          common: 'Argentina',
+        },
+      },
+    },
+  },
+];
+
+export default countriesMockData;

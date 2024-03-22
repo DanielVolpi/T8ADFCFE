d6145a4770c175957891ba6bc3d0c3514634308f
Dann Oak <dann.inbox+github@proton.me>
2024-03-22 07:24:28 -0300

    chore: updarting Styling

--- src/components/RegistrationSection/style.tsx
+++ src/components/RegistrationSection/style.tsx
@@ -1,25 +1,30 @@
 import styled from 'styled-components';
 
 import { mediaQueries } from '@/configuration/mediaQueryData';
+import { themeData } from '@/configuration/themeData';
 
 const StyledRegistrationSection = styled.div`
   max-width: 463px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 40px;
+  margin-top: 20px;
+  background-color: ${themeData.paleGrey};
 
   & > :first-child {
-    margin-bottom: 40px;
+    margin-bottom: 30px;
   }
 
   ${mediaQueries.smallResolution} {
+    padding: 20px 0 40px 0;
   }
 
   ${mediaQueries.mediumResolution} {
+    padding: 20px 0 50px 0;
   }
 
   ${mediaQueries.largeResolution} {
+    margin-bottom: 180px;
   }
 `;
 
